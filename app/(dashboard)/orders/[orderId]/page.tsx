75e6827f76f6e9585c2c39a1e5ebbaa6b7958eca
Hamza <135211820+Hamza-Amjd@users.noreply.github.com>
2025-01-27 00:38:50 +0500

    order details updated

--- app/(dashboard)/orders/[orderId]/page.tsx
+++ app/(dashboard)/orders/[orderId]/page.tsx
@@ -5,8 +5,7 @@ const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
   const res = await fetch(`${process.env.ADMIN_DASHBOARD_URL}/api/orders/${params.orderId}`)
   const { orderDetails, customer } = await res.json()
 
-  const { street, city, state, postalCode, country } = orderDetails.shippingAddress
-
+  const shippingAddress = orderDetails.shippingAddress
   return (
     <div className="flex flex-col p-10 gap-5">
       <p className="text-base-bold">
@@ -16,10 +15,13 @@ const OrderDetails = async ({ params }: { params: { orderId: string }}) => {
         Customer name: <span className="text-base-medium">{customer.name}</span>
       </p>
       <p className="text-base-bold">
-        Shipping address: <span className="text-base-medium">{street}, {city}, {state}, {postalCode}, {country}</span>
+        Shipping address: <span className="text-base-medium">{shippingAddress.line1}, {shippingAddress.city}, {shippingAddress.country}</span>
       </p>
       <p className="text-base-bold">
-        Total Paid: <span className="text-base-medium">${orderDetails.totalAmount}</span>
+        Phone number: <span className="text-base-medium">Rs. {shippingAddress.phone}</span>
+      </p>
+      <p className="text-base-bold">
+        Total Paid: <span className="text-base-medium">Rs. {orderDetails.totalAmount}</span>
       </p>
       <p className="text-base-bold">
         Shipping rate ID: <span className="text-base-medium">{orderDetails.shippingRate}</span>
