8900a75ddd51d9ccabde37988c3187b99cdb3fc6
Hamza <hamzaamjad294@gmail.com>
2025-02-24 02:38:05 +0500

    added scan route

---
+++ app/api/scan/[barcode]/route.tsx
@@ -0,0 +1,18 @@
+import Product from "@/lib/models/Product";
+import { connectToDB } from "@/lib/mongoDB";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest, { params }: { params: { barcode: number } }) => {
+  try {
+    await connectToDB()
+    console.log(params.barcode)
+    let searchedProducts = await Product.find({ barcode: params.barcode})
+
+    return NextResponse.json(searchedProducts, { status: 200 })
+  } catch (err) {
+    console.log("[search_GET]", err)
+    return new NextResponse("Internal Server Error", { status: 500 })
+  }
+}
+
+export const dynamic = "force-dynamic";
--- lib/models/Product.ts
+++ lib/models/Product.ts
@@ -6,6 +6,7 @@ const ProductSchema = new mongoose.Schema({
   media: [String],
   category: String,
   collections: [{ type: mongoose.Schema.Types.ObjectId, ref: "Collection" }],
+  barcode:Number,
   tags: [String],
   sizes: [String],
   colors: [String],
